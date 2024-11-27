603394a7d0b9cab17d3b7a9d73cfa70a6291459f
Tim Jaeger <tim.jaeger1@stud.tu-darmstadt.de>
2024-11-27 14:59:18 +0000

    Code für JSON Management und API fertig Tests fehlen
    Co-authored-by: fbender27 <fbender27@users.noreply.github.com>

--- src/prcontrol/controller/configuration.py
+++ src/prcontrol/controller/configuration.py
@@ -1,8 +1,15 @@
+#This file contains all the attrs classes, which represent all the diffrent JSON Files
+#for hardware configuration of the reactor
+
 import json
 from abc import ABC, abstractmethod
 
 import attrs
 from cattr import structure, unstructure
+
+from enum import StrEnum
+
+workspace_dir = "../../workspace/"
 
 
 class JSONSeriablizable(ABC):
@@ -15,12 +22,47 @@ class JSONSeriablizable(ABC):
     def to_json(self) -> str:
         return json.dumps(unstructure(self))
 
+    @abstractmethod
+    def get_working_dir() -> str:
+        pass
 
 @attrs.frozen(slots=True)
 class EmmissionPair(JSONSeriablizable):
     wavelength: int
     intensity: float
 
+@attrs.frozen(slots=True)
+class Event(JSONSeriablizable, StrEnum):
+    START = "START"
+    TEMPERATURE_EXCEEDS_THRESHOLD = "TEMPERATURE_EXCEEDS_THRESHOLD"
+    SAMPLE_TAKEN = "SAMPLE_TAKEN"
+    EXPERIMENT_INTERRUPTED = "EXPERIMENT_INTERRUPTED"
+    EXPERIMENT_CANCELLED = "EXPERIMENT_CANCELLED"
+    VOLTAGE_ERROR = "VOLTAGE_ERROR"
+    WATER_ERROR = "WATER_ERROR"
+
+@attrs.frozen(slots=True)
+class EventPair(JSONSeriablizable):
+    timepoint: float
+    event: Event
+
+@attrs.frozen(slots=True)
+class MeasuredDataAtTimePoint(JSONSeriablizable):
+    timepoint: float
+    temperature_thermocouple: float
+    ambient_temp_strombox: float
+    ambient_temp_photobox: float
+    voltage_lane1: float
+    current_lane1: float
+    ir_temp_lane1: float
+    voltage_lane2: float
+    current_lane2: float
+    ir_temp_lane2: float
+    voltage_lane3: float
+    current_lane3: float
+    ir_temp_lane3: float
+    uv_index: float
+    ambient_light: float
 
 @attrs.frozen(slots=True)
 class LED(JSONSeriablizable):
@@ -37,5 +79,85 @@ class LED(JSONSeriablizable):
     soldered_by: str
     operating_time: float
     defect: bool
-    emission_spectrum: tuple[EmmissionPair, EmmissionPair]
+    emission_spectrum: list[EmmissionPair]
     emission_spectrum_recorded_on: str
+
+    def get_working_dir() -> str:
+        return workspace_dir + "leds/"
+
+
+@attrs.frozen(slots=True)
+class TinkerforgeBricklet(JSONSeriablizable):
+    uid: int
+    name: str
+    version: str
+    defective: bool
+    date_bought: str
+
+    def get_working_dir() -> str:
+        return workspace_dir + "tinkerforge_bricklets/"
+
+
+@attrs.frozen(slots=True)
+class ConfigFile(JSONSeriablizable):
+    uid: int
+    tinkerforge_bricklets: list[TinkerforgeBricklet]
+    software_version: str
+    date: str
+    default_distance_led_vial: float
+    default_position_thermocouple: str
+    default_pwm_channels: list[float]
+    configuration_io_channels: list
+    default_temperature_threshold: float
+    default_uv_threshold: float
+    default_sensor_query_interval: float
+    default_reaction_vessel_volume: float
+
+    def get_working_dir() -> str:
+        return workspace_dir + "config_files/"
+
+@attrs.frozen(slots=True)
+class ExperimentTemplate(JSONSeriablizable):
+    uid: int
+    date: str
+    config_file: ConfigFile
+    active_lane: int
+    led_front: LED
+    led_front_intensity: int
+    led_front_distance_to_vial: float
+    led_front_exposure_time: float
+    led_back: LED
+    led_back_intensity: int
+    led_back_distance_to_vial: float
+    led_back_exposure_time: float
+    time_points_sample_taking: list[int]
+    position_thermocouple: str
+
+    def get_working_dir() -> str:
+        return workspace_dir + "experiment_templates/"
+
+@attrs.frozen(slots=True)
+class Experiment(JSONSeriablizable):
+    uid: int
+    date: str
+    config_file: ConfigFile
+    active_lane: int
+    led_front: LED
+    led_front_intensity: int
+    led_front_distance_to_vial: float
+    led_front_exposure_time: float
+    led_back: LED
+    led_back_intensity: int
+    led_back_distance_to_vial: float
+    led_back_exposure_time: float
+    time_points_sample_taking: list[int]
+    size_sample: float
+    parallel_experiments: list[int]
+    position_thermocouple: str
+    error_occured: bool
+    experiment_cancelled: bool
+    event_log: list[EventPair]
+    measured_data: list[MeasuredDataAtTimePoint]
+
+    def get_working_dir() -> str:
+        return workspace_dir + "experiments/"
--- src/prcontrol/webapi/api.py
+++ src/prcontrol/webapi/api.py
@@ -2,9 +2,11 @@ from flask import Flask, request
 from flask.typing import ResponseReturnValue
 from flask_cors import CORS
 
-from prcontrol.controller import configuration
+from prcontrol.controller import configuration, config_manager
+
 
 app = Flask(__name__)
+config_manager = ConfigManager()
 CORS(app)
 
 
@@ -12,13 +14,193 @@ CORS(app)
 def index() -> ResponseReturnValue:
     return "<h1> Hello World! </h1>", 200
 
+@app.route("/get_led", methods=["GET"])
+def get_led() -> ResponseReturnValue:
 
-@app.route("/upload_file", methods=["POST"])
-def uploade_config() -> ResponseReturnValue:
-    print(f"Received request: {request}")
+    uid = request.args.get('uid');
+    if (uid == None)
+        return "get_led expects argument uid", 400
+
+    json = config_manager.load_led(uid).to_json()
+
+    return json, 200
+
+@app.route("/get_available_led_uids", methods=["GET"])
+def get_available_led_uids() -> ResponseReturnValue:
+    
+    led_uids = json.dumps(config_manager.leds)
+    return led_uids, 200
+
+@app.route("/get_bricklet", methods=["GET"])
+def get_bricklet() -> ResponseReturnValue:
+
+    uid = request.args.get('uid');
+    if (uid == None)
+        return "get_bricklet expects argument uid", 400
+
+    json = config_manager.load_bricklet(uid).to_json()
+
+    return json, 200
+
+@app.route("/get_available_bricklet_uids", methods=["GET"])
+def get_available_bricklet_uids() -> ResponseReturnValue:
+    
+    bricklet_uids = json.dumps(config_manager.bricklets)
+    return bricklet_uids, 200
+
+@app.route("/get_config", methods=["GET"])
+def get_config() -> ResponseReturnValue:
+
+    uid = request.args.get('uid');
+    if (uid == None)
+        return "get_config expects argument uid", 400
+
+    json = config_manager.load_config(uid).to_json()
+
+    return json, 200
+
+@app.route("/get_available_config_uids", methods=["GET"])
+def get_available_config_uids() -> ResponseReturnValue:
+    
+    config_uids = json.dumps(config_manager.configs)
+    return config_uids, 200
+
+@app.route("/get_experiment_template", methods=["GET"])
+def get_experiment_template() -> ResponseReturnValue:
+
+    uid = request.args.get('uid');
+    if (uid == None)
+        return "get_experiment_template expects argument uid", 400
+
+    json = config_manager.load_experiment_template(uid).to_json()
+
+    return json, 200
+
+@app.route("/get_available_experiment_template_uids", methods=["GET"])
+def get_available_experiment_template_uids() -> ResponseReturnValue:
+    
+    experiment_template_uids = json.dumps(config_manager.exp_temps)
+    return experiment_template_uids, 200
+
+@app.route("/get_experiment", methods=["GET"])
+def get_experiment() -> ResponseReturnValue:
+
+    uid = request.args.get('uid');
+    if (uid == None)
+        return "get_experiment expects argument uid", 400
+
+    json = config_manager.load_experiment(uid).to_json()
+
+    return json, 200
+
+@app.route("/get_available_experiments_uids", methods=["GET"])
+def get_available_experiment_uids() -> ResponseReturnValue:
+    
+    experiment_uids = json.dumps(config_manager.exps)
+    return experiment_uids, 200
+
+@app.route("/delete_led", methods=["GET"])
+def delete_led() -> ResponseReturnValue:
+
+    uid = request.args.get('uid');
+    if (uid == None)
+        return "delete_led expects argument uid", 400
+
+    config_manager.delete_led(uid)
+
+    return "LED deleted", 200
+
+@app.route("/delete_bricklet", methods=["GET"])
+def delete_bricklet() -> ResponseReturnValue:
+
+    uid = request.args.get('uid');
+    if (uid == None)
+        return "delete_bricklet expects argument uid", 400
+
+    config_manager.delete_bricklet(uid)
+
+    return "Bricklet deleted", 200
+
+@app.route("/delete_config", methods=["GET"])
+def delete_config() -> ResponseReturnValue:
+
+    uid = request.args.get('uid');
+    if (uid == None)
+        return "delete_config expects argument uid", 400
+
+    config_manager.delete_config(uid)
+
+    return "Config deleted", 200
+
+@app.route("/delete_experiment_template", methods=["GET"])
+def delete_experiment_template() -> ResponseReturnValue:
+
+    uid = request.args.get('uid');
+    if (uid == None)
+        return "delete_experiment_template expects argument uid", 400
+
+    config_manager.delete_experiment_template(uid)
+
+    return "Template deleted", 200
+
+@app.route("/delete_experiment", methods=["GET"])
+def delete_experiment() -> ResponseReturnValue:
+
+    uid = request.args.get('uid');
+    if (uid == None)
+        return "delete_experiment expects argument uid", 400
+
+    config_manager.delete_experiment(uid)
+
+    return "Experiment deleted", 200
+
+@app.route("/upload_led", methods=["POST"])
+def upload_led() -> ResponseReturnValue:
+
     file = request.files["file"]
     if not file:
-        return "upload_files expects a file", 400
+        return "upload_led expects a file", 400
 
-    print(f"Received LED: {configuration.LED.from_json(file.stream.read())}")
-    return "TOP!", 200
+    config_manager.add_led(file.stream.read())
+    return "Received LED", 200
+    
+@app.route("/upload_config", methods=["POST"])
+def upload_config() -> ResponseReturnValue:
+
+    file = request.files["file"]
+    if not file:
+        return "upload_config expects a file", 400
+
+    config_manager.add_config(file.stream.read())
+    return "Received Config", 200
+
+@app.route("/upload_bricklet", methods=["POST"])
+def upload_bricklet() -> ResponseReturnValue:
+
+    file = request.files["file"]
+    if not file:
+        return "upload_bricklet expects a file", 400
+
+    config_manager.add_bricklet(file.stream.read())
+    return "Received Bricklet", 200
+
+@app.route("/upload_experiment_template", methods=["POST"])
+def upload_experiment_template() -> ResponseReturnValue:
+
+    file = request.files["file"]
+    if not file:
+        return "upload_experiment_template expects a file", 400
+
+    config_manager.add_experiment_template(file.stream.read())
+    return "Received Template", 200
+
+
+#@app.route("/upload_file", methods=["POST"])
+#def uploade_config() -> ResponseReturnValue:
+#    print(f"Received request: {request}")
+#    file = request.files["file"]
+#    if not file:
+#        return "upload_files expects a file", 400
+#
+#    print(f"Received LED: {configuration.LED.from_json(file.stream.read())}")
+#    return "TOP!", 200
