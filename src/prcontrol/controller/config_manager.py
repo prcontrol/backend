4a8899a2cad381fec8f9b4240f3febcd834ede76
tim-jaeger <66910379+tim-jaeger@users.noreply.github.com>
2024-12-10 13:27:01 +0100

    Configuration storing exporting (#22)
    Added Python Representation of JSON Objects.
    Added ConfigFolders for Managing JSON Objects stored in Backend.
    Added API Call for accessing and uploading JSON Objects.
    
    ---------
    Co-authored-by: fbender27 <fbender27@users.noreply.github.com>
    Co-authored-by: Henri Dohmen <henri.dohmen@stud.tu-darmstadt.de>

---
+++ src/prcontrol/controller/config_manager.py
@@ -0,0 +1,110 @@
+# This file contains the tools for managing and
+# loading stored configuration files
+
+import os
+import pathlib
+import re
+from collections.abc import Iterable
+
+from attrs import define, field
+
+from prcontrol.controller.configuration import (
+    LED,
+    ConfigObject,
+    Experiment,
+    ExperimentTemplate,
+    HardwareConfig,
+    TinkerforgeBricklet,
+)
+
+
+@define
+class ConfigFolder[T: ConfigObject]:
+    workspace: pathlib.Path = field(converter=pathlib.Path)
+    kind: type[T]
+
+    _configs: set[int] = field(init=False, factory=set)
+    _FILENAME_PATTERN = re.compile(r"obj_([0-9]+)\.json")
+
+    def __attrs_post_init__(self) -> None:
+        if not os.path.isdir(self.workspace):
+            os.makedirs(self.workspace)
+        self.update()
+
+    def _path_of_uid(self, uid: int) -> pathlib.Path:
+        return self.workspace / f"obj_{uid}.json"
+
+    def update(self) -> None:
+        """Update configuration files from disk."""
+        for file_path in os.listdir(self.workspace):
+            _match = self._FILENAME_PATTERN.fullmatch(file_path)
+            if _match is None:
+                continue
+
+            id = int(_match.groups()[0])
+            self._configs.add(id)
+
+    def load(self, uid: int) -> ConfigObject:
+        """Get configuration with given `uid`.
+
+        Throws `FileNotFoundError` if there is no configuration with the given
+        `uid`.
+        """
+        if uid not in self._configs:
+            raise FileNotFoundError(
+                "Config for {self.name} with uid {uid} not found."
+            )
+
+        with open(self._path_of_uid(uid)) as config:
+            return self.kind.from_json(config.read())
+
+    def add(self, config_object: T) -> None:
+        """Store configuration under the uid `uid`.
+        May overwrite the given `uid`.
+        """
+        path = self._path_of_uid(config_object.get_uid())
+        with open(path, "w") as file:
+            file.write(config_object.to_json())
+
+            self._configs.add(config_object.get_uid())
+
+    def add_from_json(self, config_json: str | bytes | bytearray) -> None:
+        """Store configuration under the uid `uid`.
+        May overwrite the given `uid`.
+        Validates the json string's structure.
+        """
+        self.add(self.kind.from_json(config_json))
+
+    def delete(self, uid: int) -> None:
+        """Delete configuration `id` if exists"""
+        if uid in self._configs:
+            os.remove(self._path_of_uid(uid))
+            self._configs.remove(uid)
+
+    def load_all(self) -> Iterable[ConfigObject]:
+        self.update()
+        for uid in self._configs:
+            yield self.load(uid)
+
+
+class ConfigManager:
+    leds: ConfigFolder[LED]
+    bricklets: ConfigFolder[TinkerforgeBricklet]
+    experiment_templates: ConfigFolder[ExperimentTemplate]
+    experiments: ConfigFolder[Experiment]
+    configs: ConfigFolder[HardwareConfig]
+
+    def __init__(self, base_path: str | pathlib.Path | None = None) -> None:
+        if base_path is None:
+            base_path = "./workspace"
+        base_path = pathlib.Path(base_path)
+
+        self.leds = ConfigFolder(base_path / "leds", LED)
+        self.bricklets = ConfigFolder(
+            base_path / "bricklets", TinkerforgeBricklet
+        )
+        self.experiment_templates = ConfigFolder(
+            base_path / "exp_tmps", ExperimentTemplate
+        )
+        self.experiments = ConfigFolder(base_path / "experiments", Experiment)
+        self.configs = ConfigFolder(base_path / "configs", HardwareConfig)
--- src/prcontrol/webapi/api.py
+++ src/prcontrol/webapi/api.py
@@ -1,11 +1,16 @@
-from flask import Flask, request
+import json
+from typing import Any
+
+from flask import Flask, Request, request
 from flask.typing import ResponseReturnValue
 from flask_cors import CORS
 
-from prcontrol.controller import configuration
+from prcontrol.controller.config_manager import ConfigFolder, ConfigManager
 
 app = Flask(__name__)
 CORS(app)
+
+config_manager = ConfigManager()
 
 
 @app.route("/", methods=["GET"])
@@ -13,12 +18,110 @@ def index() -> ResponseReturnValue:
     return "<h1> Hello World! </h1>", 200
 
 
-@app.route("/upload_file", methods=["POST"])
-def uploade_config() -> ResponseReturnValue:
-    print(f"Received request: {request}")
-    file = request.files["file"]
-    if not file:
-        return "upload_files expects a file", 400
+@app.route("/led", methods=["GET", "POST", "DELETE"])
+def led() -> ResponseReturnValue:
+    return handle_config_api(config_manager.leds, request)
 
-    print(f"Received LED: {configuration.LED.from_json(file.stream.read())}")
-    return "TOP!", 200
+
+@app.route("/bricklet", methods=["GET"])
+def bricklet() -> ResponseReturnValue:
+    return handle_config_api(config_manager.bricklets, request)
+
+
+@app.route("/exp_tmp", methods=["GET", "POST", "DELETE"])
+def exp_tmp() -> ResponseReturnValue:
+    return handle_config_api(config_manager.experiment_templates, request)
+
+
+@app.route("/config", methods=["GET", "POST", "DELETE"])
+def config() -> ResponseReturnValue:
+    return handle_config_api(config_manager.configs, request)
+
+
+@app.route("/experiment", methods=["GET", "DELETE"])
+def experiment() -> ResponseReturnValue:
+    return handle_config_api(config_manager.experiments, request)
+
+
+@app.route("/list_led", methods=["GET"])
+def list_leds() -> ResponseReturnValue:
+    return handle_list_api(config_manager.leds, request)
+
+
+@app.route("/list_bricklet", methods=["GET"])
+def list_bricklets() -> ResponseReturnValue:
+    return handle_list_api(config_manager.bricklets, request)
+
+
+@app.route("/list_exp_tmp", methods=["GET"])
+def list_exp_tmps() -> ResponseReturnValue:
+    return handle_list_api(config_manager.experiment_templates, request)
+
+
+@app.route("/list_config", methods=["GET"])
+def list_configs() -> ResponseReturnValue:
+    return handle_list_api(config_manager.configs, request)
+
+
+@app.route("/list_experiment", methods=["GET"])
+def list_experiments() -> ResponseReturnValue:
+    return handle_list_api(config_manager.experiments, request)
+
+
+def handle_config_api(
+    folder: ConfigFolder[Any], request: Request
+) -> ResponseReturnValue:
+    if request.method == "POST":
+        print(request)
+        file = request.files["file"]
+        if not file:
+            return "post expects a file", 400
+        folder.add_from_json(file.stream.read())
+        return "success", 200
+
+    elif request.method == "GET":
+        _uid = request.args.get("uid")
+        if not _uid:
+            return "get expects argument uid", 400
+
+        try:
+            uid = int(_uid)
+        except ValueError:
+            return "uid must be integral", 400
+
+        try:
+            config = folder.load(uid)
+            return config.to_json(), 200
+        except FileNotFoundError:
+            return "file does not exist", 400
+
+    elif request.method == "DELETE":
+        _uid = request.args.get("uid")
+        if not _uid:
+            return "delete expects argument uid", 400
+
+        try:
+            uid = int(_uid)
+        except ValueError:
+            return "uid must be integral", 400
+
+        folder.delete(uid)
+        return "success", 200
+
+    raise RuntimeError("We should never get here")
+
+
+def handle_list_api(
+    folder: ConfigFolder[Any], request: Request
+) -> ResponseReturnValue:
+    assert request.method == "GET"
+
+    list_of_configs = [
+        {
+            "uid": config_object.get_uid(),
+            "description": config_object.get_description(),
+        }
+        for config_object in folder.load_all()
+    ]
+
+    return json.dumps({"results": list_of_configs}), 200
