8c504fdb95fa2d39a909c92a6a73853fd2be1203
tim-jaeger <66910379+tim-jaeger@users.noreply.github.com>
2024-12-19 17:24:25 +0100

    Testing and cleanup (#30)
    * Added tests and adapted src
    
    * test_config_folder finished
    
    * Added Tests for api and corrected bugs
    
    * Tests for LED Routes finished
    
    * Changed config_manager to private attribut and specified Exception
    
    * [tests] rename some stuff, fix clean s.t. it actually uses its argument
    
    * [tests] rename
    
    ---------
    
    Co-authored-by: Henri Dohmen <henri.dohmen@stud.tu-darmstadt.de>

---
+++ tests/test_api.py
@@ -0,0 +1,149 @@
+import io
+import json
+
+import pytest
+
+from prcontrol.controller.config_manager import ConfigFolder
+from prcontrol.controller.configuration import (
+    LED,
+    EmmissionPair,
+)
+from prcontrol.webapi.api import app, config_manager
+from tests.test_config_folder import clean_directory
+
+
+def create_mock_led(id: int, desc: str) -> LED:
+    return LED(
+        uid=id,
+        name=desc,
+        fwhm=1,
+        max_of_emission=2,
+        min_wavelength=3,
+        max_wavelength=4,
+        color="blue",
+        max_current=5,
+        manufacturer_id=6,
+        order_id=7,
+        date_soldering="2024-01-01",
+        soldered_by="Tim",
+        operating_time=8.0,
+        defect=False,
+        emission_spectrum=(
+            EmmissionPair(wavelength=9, intensity=10.0),
+            EmmissionPair(wavelength=11, intensity=12.0),
+        ),
+        emission_spectrum_recorded_on="2023-01-01",
+    )
+
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+
+@pytest.fixture
+def clean_environment():
+    test_dir = config_manager.leds
+    clean_directory(test_dir.workspace)
+    yield test_dir
+    clean_directory(test_dir.workspace)
+
+
+def init_dir_with_n_leds(num_elements: int, dir: ConfigFolder):
+    for i in range(num_elements):
+        obj = create_mock_led(i, f"default_obj_{i}")
+        dir.add(obj)
+
+
+def test_availability(client):
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"<h1> Hello World! </h1>" in response.data
+
+
+def test_list_api(client, clean_environment):
+    init_dir_with_n_leds(2, clean_environment)
+    response = client.get("/list_led")
+    assert response.status_code == 200
+    return_obj = json.loads(response.data)
+    assert len(return_obj["results"]) == 2
+    assert return_obj["results"][0]["uid"] == 0
+    assert (
+        return_obj["results"][0]["description"]
+        == clean_environment.load(0).get_description()
+    )
+    assert return_obj["results"][1]["uid"] == 1
+    assert (
+        return_obj["results"][1]["description"]
+        == clean_environment.load(1).get_description()
+    )
+
+
+def test_config_api_GET_normal(client, clean_environment):
+    init_dir_with_n_leds(1, clean_environment)
+    response = client.get("/led", query_string=dict(uid=0))
+    assert response.status_code == 200
+    rec_obj = LED.from_json(response.data)
+    assert rec_obj == clean_environment.load(0)
+
+
+def test_config_api_GET_no_uid(client):
+    response = client.get("/led")
+    assert response.status_code == 400
+    assert response.data == b"get expects argument uid"
+
+
+def test_config_api_GET_uid_as_str(client):
+    response = client.get("/led", query_string=dict(uid="test"))
+    assert response.status_code == 400
+    assert response.data == b"uid must be integer"
+
+
+def test_config_api_GET_no_file(client, clean_environment):
+    init_dir_with_n_leds(3, clean_environment)
+    response = client.get("/led", query_string=dict(uid=40))
+    assert response.status_code == 400
+    assert response.data == b"file does not exist"
+
+
+def test_config_api_POST_new_file(client, clean_environment):
+    obj = create_mock_led(0, "test")
+    data = {"json_file": (io.BytesIO(obj.to_json().encode()), "test.json")}
+    response = client.post("/led", data=data)
+    assert response.status_code == 200
+    assert obj == clean_environment.load(0)
+
+
+def test_config_api_POST_no_file(client):
+    obj = create_mock_led(0, "test")
+    data = {"file": (io.BytesIO(obj.to_json().encode()), "test.json")}
+    response = client.post("/led", data=data)
+    assert response.status_code == 400
+    assert response.data == b"post expects a json_file"
+
+
+def test_config_api_DELETE_normal(client, clean_environment):
+    init_dir_with_n_leds(10, clean_environment)
+    response = client.delete("/led", query_string=dict(uid=5))
+    assert response.status_code == 200
+    assert len(clean_environment._configs) == 9
+
+
+def test_config_api_DELETE_no_file(client, clean_environment):
+    init_dir_with_n_leds(10, clean_environment)
+    response = client.delete("/led", query_string=dict(uid=42))
+    assert response.status_code == 200
+    assert len(clean_environment._configs) == 10
+
+
+def test_config_api_DELETE_no_uid(client):
+    response = client.delete("/led")
+    assert response.status_code == 400
+    assert response.data == b"delete expects argument uid"
+
+
+def test_config_api_DELETE_uid_as_str(client):
+    response = client.delete("/led", query_string=dict(uid="test"))
+    assert response.status_code == 400
+    assert response.data == b"uid must be integer"
--- tests/test_config_folder.py
+++ tests/test_config_folder.py
@@ -1,7 +1,9 @@
 import os
+import pathlib
 import shutil
 
 import attrs
+import pytest
 
 from prcontrol.controller.config_manager import ConfigFolder
 from prcontrol.controller.configuration import ConfigObject
@@ -19,6 +21,13 @@ class MyConfigTestObject(ConfigObject):
         return self.name
 
 
+def clean_directory(dir: str):
+    dir = pathlib.Path(dir)
+    if os.path.isdir(dir):
+        shutil.rmtree(dir)
+    os.mkdir(dir)
+
+
 def init_test_folder(
     num_elements: int, workspace: str
 ) -> ConfigFolder[MyConfigTestObject]:
@@ -26,97 +35,80 @@ def init_test_folder(
 
     for i in range(num_elements):
         obj = MyConfigTestObject(uid=i, name=f"default_obj_{i}")
-        dir.add_from_json(obj.to_json())
+        dir.add(obj)
 
     return dir
 
 
-def test_creation_of_new_folder():
-    if os.path.isdir("./test1/"):
-        shutil.rmtree("./test1/")
-    dir = init_test_folder(0, "./test1/")
+@pytest.fixture
+def dir_path():
+    dir_path = "./test/"
+    clean_directory(dir_path)
+    yield dir_path
+    clean_directory(dir_path)
+
+
+def test_creation_of_new_folder(dir_path):
+    dir = init_test_folder(0, dir_path)
     assert len(dir._configs) == 0
-    shutil.rmtree("./test1/")
+    assert len(os.listdir(dir.workspace)) == 0
 
 
-def test_opening_existing_folder():
-    if os.path.isdir("./test2/"):
-        shutil.rmtree("./test2/")
-    dir = init_test_folder(4, "./test2/")
-    assert len(dir._configs) == 4
-    shutil.rmtree("./test2/")
+def test_opening_existing_folder(dir_path):
+    wrk_spc = init_test_folder(4, dir_path).workspace
+    new_dir = ConfigFolder(wrk_spc, MyConfigTestObject)
+    assert len(new_dir._configs) == 4
 
 
-def test_adding_json():
-    if os.path.isdir("./test3/"):
-        shutil.rmtree("./test3/")
-
-    dir = init_test_folder(0, "./test3/")
-
+def test_adding_json(dir_path):
+    dir = init_test_folder(0, dir_path)
     obj = MyConfigTestObject(0, "new object")
     dir.add(obj)
     assert len(dir._configs) == 1
     assert len(os.listdir(dir.workspace)) == 1
-    shutil.rmtree("./test3/")
 
 
-def test_loading_json():
-    if os.path.isdir("./test4/"):
-        shutil.rmtree("./test4/")
-
-    dir = init_test_folder(0, "./test4/")
-
+def test_loading_json(dir_path):
+    dir = init_test_folder(0, dir_path)
     obj = MyConfigTestObject(0, "new object")
     dir.add(obj)
     loaded_obj = dir.load(0)
-
     assert loaded_obj == MyConfigTestObject(uid=0, name="new object")
-    shutil.rmtree("./test4/")
 
 
-def test_overwriting_json():
-    if os.path.isdir("./test5/"):
-        shutil.rmtree("./test5/")
-    dir = init_test_folder(1, "./test5/")
+def test_overwriting_json(dir_path):
+    dir = init_test_folder(1, dir_path)
     obj = MyConfigTestObject(0, "new name")
     dir.add(obj)
-
-    assert len(list(dir.load_all())) == 1
+    assert len(dir._configs) == 1
     assert len(os.listdir(dir.workspace)) == 1
-
     loaded_obj = dir.load(0)
     assert loaded_obj.name == "new name"
-    shutil.rmtree("./test5/")
 
 
-def test_load_not_existing_json():
-    if os.path.isdir("./test6/"):
-        shutil.rmtree("./test6/")
-    dir = init_test_folder(2, "./test6/")
-
+def test_load_not_existing_json(dir_path):
+    dir = init_test_folder(2, dir_path)
     try:
         _ = dir.load(5)
         raise AssertionError()
     except FileNotFoundError:
         assert True
-        shutil.rmtree("./test6/")
 
 
-def test_deleting_json():
-    if os.path.isdir("./test7/"):
-        shutil.rmtree("./test7/")
-    dir = init_test_folder(1, "./test7/")
+def test_deleting_json(dir_path):
+    dir = init_test_folder(1, dir_path)
     dir.delete(0)
     assert len(dir._configs) == 0
     assert len(os.listdir(dir.workspace)) == 0
-    shutil.rmtree("./test7/")
 
 
-def test_deleting_not_existing_json():
-    if os.path.isdir("./test8/"):
-        shutil.rmtree("./test8/")
-    dir = init_test_folder(5, "./test8/")
+def test_deleting_not_existing_json(dir_path):
+    dir = init_test_folder(5, dir_path)
     dir.delete(40)
     assert len(dir._configs) == 5
     assert len(os.listdir(dir.workspace)) == 5
-    shutil.rmtree("./test8/")
+
+
+def test_list_all_json(dir_path):
+    dir = init_test_folder(5, dir_path)
+    assert len(list(dir.load_all())) == 5
